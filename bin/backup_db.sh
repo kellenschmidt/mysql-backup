0d9f593c934b0c018318ee368877ce5a0f232006
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-07-08 23:14:24 -0500

    Set host in mysqldump to hostname env var

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -4,4 +4,4 @@
 # $1: Time period type of backup
 
 date=$(date +"%d-%m-%Y_%H:%M:%S")
-mysqldump -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD --lock-tables=false --add-drop-database --add-drop-table --databases personal_website | gzip > /home/backups/mysql-${ENV}-$1_$date.sql.gz
+mysqldump -h$HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --lock-tables=false --add-drop-database --add-drop-table --databases personal_website | gzip > /home/backups/mysql-${ENV}-$1_$date.sql.gz
