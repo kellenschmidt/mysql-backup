ecdaa5190fa406389cfa53ee2a41cc5c3f38093f
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-07-12 22:52:23 -0500

    Use start and finish urls to monitor cron job run time

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -4,10 +4,17 @@
 # $1: Time period type of backup
 
 date=$(date +"%d-%m-%Y_%H:%M:%S")
+
+if [ "$ENV" == "kellenschmidtcom" ]; then
+  curl -fsS --retry 3 https://cronhub.io/start/b664ebe0-8646-11e8-a0d4-d38e284fbc15
+elif [ "$ENV" == "kellenforthewin" ]; then
+  curl -fsS --retry 3 https://cronhub.io/start/92789fd0-8647-11e8-95a2-7352beb5f5da
+fi
+
 mysqldump -h$HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --lock-tables=false --add-drop-database --add-drop-table --databases personal_website | gzip > /home/backups/mysql-${ENV}-$1_$date.sql.gz
 
 if [ "$ENV" == "kellenschmidtcom" ]; then
-  curl -fsS --retry 3 https://cronhub.io/ping/b664ebe0-8646-11e8-a0d4-d38e284fbc15
+  curl -fsS --retry 3 https://cronhub.io/finish/b664ebe0-8646-11e8-a0d4-d38e284fbc15
 elif [ "$ENV" == "kellenforthewin" ]; then
-  curl -fsS --retry 3 https://cronhub.io/ping/92789fd0-8647-11e8-95a2-7352beb5f5da
+  curl -fsS --retry 3 https://cronhub.io/finish/92789fd0-8647-11e8-95a2-7352beb5f5da
 fi
